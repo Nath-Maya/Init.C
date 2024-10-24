df36557f036bf955250258f880e26a878cc5e450
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-10-23 19:06:54 -0500

    activity 19,21,23

--- PCV01/Actividad-2/dot-18.c
+++ PCV01/Actividad-2/dot-18.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-main() {
-    int x, y;
-    x = y = 0;
-    printf("El resultado de la comparación es: %d\n", x = y);
-    return 0;
+int main()
+{
+   int x, y;
+   x = y = 0;
+   printf("El resultado de la comparación es: %d\n", x = y);
+   return 0;
 }
---
+++ PCV01/Actividad-2/dot-19.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1; 
+    do {
+        printf("%d\n", i); //Codigo a ejecutar
+        i++; 
+    } while (i <= 5); // Condicion a evaluar
+    return 0;
+}
