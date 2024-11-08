56024bc02aaf541a4b598994f753549e1dc89244
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-11-07 22:19:31 -0500

    Refactor: Move the loop to re-display the menu inside the main function, ensuring proper flow. Added input validation for the user option. Improved string input handling by replacing  with  to allow reading full lines. Enhanced clarity in the exchange values function with additional comments. Optimized prime number checking logic.

--- PCV01/Actividad-IV/tools.c
+++ PCV01/Actividad-IV/tools.c
@@ -16,49 +16,62 @@ int main()
 
    //** Definir la variable que contendra la funcion
    int option;
+   char back;
 
-   //**  Imprimo las opciones para que el usuario seleccione
-   printf("TOOLS EN C \n\n");
-
-   printf("1. Calcular el promedio de un arreglo de enteros\n");
-   printf("2. Invertir una cadena de caracteres\n");
-   printf("3. Verificar si un número es primo\n");
-   printf("4. Contar vocales y consonantes en una cadena\n");
-   printf("5. Calcular el factorial de un número\n");
-   printf("6. Intercambiar los valores de dos enteros\n\n");
-   printf("Seleccione la utilidad o herramienta que necesita usar: \n");
-   scanf("%d", &option);
-
-   //* Estructura de Control de la opcion seleccionada
-   switch (option)
+   do
    {
-   case 1:
-      printf("Opcion seleccionada: Calcular el promedio de un arreglo de enteros\n");
-      calculateAverage();
+      //**  Imprimo las opciones para que el usuario seleccione
+      printf("TOOLS EN C \n\n");
 
-      break;
-   case 2:
-      printf("Opcion seleccionada: Invertir una cadena de caracteres\n");
-      invertString();
-      break;
-   case 3:
-      printf("Opcion seleccionada: Verificar si un número es primo\n");
-      isPrime();
-      break;
-   case 4:
-      printf("Opcion seleccionada: Contar vocales y consonantes en una cadena\n");
-      countVowelsConsonants();
-      break;
-   case 5:
-      printf("Opcion seleccionada: Calcular el factorial de un número\n");
-      factorial();
-      break;
-   case 6:
-      printf("Opcion seleccionada: Intercambiar los valores de dos enteros\n");
-      break;
-   default:
-      printf("Opcion Incorrecta\n");
-   }
+      printf("1. Calcular el promedio de un arreglo de enteros\n");
+      printf("2. Invertir una cadena de caracteres\n");
+      printf("3. Verificar si un número es primo\n");
+      printf("4. Contar vocales y consonantes en una cadena\n");
+      printf("5. Calcular el factorial de un número\n");
+      printf("6. Intercambiar los valores de dos enteros\n\n");
+      printf("Seleccione la utilidad o herramienta que necesita usar: \n");
+      scanf("%d", &option);
+
+      //* Estructura de Control de la opcion seleccionada
+      switch (option)
+      {
+      case 1:
+         printf("Opcion seleccionada: Calcular el promedio de un arreglo de enteros\n");
+         calculateAverage();
+
+         break;
+      case 2:
+         printf("Opcion seleccionada: Invertir una cadena de caracteres\n");
+         invertString();
+         break;
+      case 3:
+         printf("Opcion seleccionada: Verificar si un número es primo\n");
+         isPrime();
+         break;
+      case 4:
+         printf("Opcion seleccionada: Contar vocales y consonantes en una cadena\n");
+         countVowelsConsonants();
+         break;
+      case 5:
+         printf("Opcion seleccionada: Calcular el factorial de un número\n");
+         factorial();
+         break;
+      case 6:
+         printf("Opcion seleccionada: Intercambiar los valores de dos enteros\n");
+         exchangeValues();
+         break;
+      default:
+         printf("Opcion Incorrecta\n");
+      }
+
+      //! Preguntar al usuario si quiere volver al menu inicial
+
+      printf("\n¿DESEA VER EL MENU NUEVAMENTE? (s/n): ");
+      getchar(); // Limpiar campo
+      scanf("%c", &back);
+   } while (back == 's' || back == 'S');
+
+   printf("Gracias por usar TOOLS en C.\n");
 
    return 0;
 }
